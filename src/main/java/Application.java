6928b43e2ea41e1fd8931695a3ac8f6d374a7873
Saplitsyn Denis <packman17@mail.ru>
2024-02-12 20:21:02 +0300

    refactor app use statement

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,6 @@
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Application {
     public static void main(String[] args) throws SQLException {
@@ -11,18 +12,42 @@ public class Application {
                 statement.execute(sql);
             }
 
-            var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
-            try (var statement2 = conn.createStatement()) {
-                statement2.executeUpdate(sql2);
+            var sql2 = "INSERT INTO users (username, phone) VALUES (?, ?)";
+            try (var preparedStatement = conn.prepareStatement(sql2, Statement.RETURN_GENERATED_KEYS)) {
+                preparedStatement.setString(1, "Tommy");
+                preparedStatement.setString(2, "33333333");
+                preparedStatement.executeUpdate();
+                var generatedKeys1 = preparedStatement.getGeneratedKeys();
+                if (generatedKeys1.next()) {
+                    System.out.println(generatedKeys1.getLong(1));
+                } else {
+                    throw new SQLException("DB have not returned an id after saving the entity");
+                }
+
+                preparedStatement.setString(1, "Maria");
+                preparedStatement.setString(2, "44444444");
+                preparedStatement.executeUpdate();
+                var generatedKeys2 = preparedStatement.getGeneratedKeys();
+                if (generatedKeys2.next()) {
+                    System.out.println(generatedKeys2.getLong(1));
+                } else {
+                    throw new SQLException("DB have not returned an id after saving the entity");
+                }
             }
 
             var sql3 = "SELECT * FROM users";
-            try (var statement3 = conn.createStatement()) {
-                var resultSet = statement3.executeQuery(sql3);
+            try (var statement = conn.createStatement()) {
+                var resultSet = statement.executeQuery(sql3);
                 while (resultSet.next()) {
                     System.out.println(resultSet.getString("username"));
                     System.out.println(resultSet.getString("phone"));
                 }
+            }
+
+            String deleteSql = "DELETE FROM users WHERE username = ?";
+            try (var statement = conn.prepareStatement(deleteSql)) {
+                statement.setString(1, "Maria");
+                statement.executeUpdate();
             }
         }
     }
